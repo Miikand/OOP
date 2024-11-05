aa575ba6b2833ee7ee37e73c8fdd626cff899c99
Miikand <mikoand@wp.pl>
2024-11-05 08:41:56 +0100

    add 2 exercises

--- Exercises_Basics/Program.cs
+++ Exercises_Basics/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection.Metadata.Ecma335;
+
 car auto = new car("Citroen","c2","2006");
 auto.Displayinfo();
 class car(string brand, string model, string data)
@@ -10,5 +12,42 @@ class car(string brand, string model, string data)
         Console.WriteLine(brand);
         Console.WriteLine(model);
         Console.WriteLine(data);
+    }
+}
+
+class Student(string name, int grade)
+{
+    public string Name { get;
+        private set; }
+    public int grade { get { return grade; }
+        set
+        {
+            if (grade <= 6 && grade >= 1)
+            {
+                grade = value;
+            }
+
+            else
+            {
+                throw new ArgumentOutOfRangeException("Ocena musi byc z zakresu od 1 do 6");
+            }
+        }
+    }
+    public bool isPassing(){
+        if (grade >= 2)
+        {
+            return true;
+        }
+        else { return false; }
+}
+}
+class Book(string Title, string author, int pages)
+{
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public int Pages { get; private set; }
+    public void GetSummary()
+    {
+        Console.WriteLine("Title by" + author );
     }
 }
